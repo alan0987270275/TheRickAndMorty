c353684a2a41a98f6b58c5a6ebd6cb8147d88e20
alan0313 <alan0987270275@gmail.com>
2021-06-13 21:33:11 +0800

    Add Fragment and navigation component to manage the flows.

--- app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
+++ app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.example.therickandmorty
+package com.example.therickandmorty.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.example.therickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.navHostFragment).navigateUp()
+    }
+
 }
