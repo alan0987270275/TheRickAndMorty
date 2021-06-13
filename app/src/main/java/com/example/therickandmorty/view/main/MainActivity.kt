43dd29e8574691be2afef23615195c22997e32bf
alan0313 <alan0987270275@gmail.com>
2021-06-13 22:03:54 +0800

    Add customNavigator to make show and hide FragmentTransaction

--- app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
+++ app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
@@ -3,16 +3,31 @@ package com.example.therickandmorty.view.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.plusAssign
 import com.example.therickandmorty.R
+import com.example.therickandmorty.util.CustomNavigator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigation()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.navHostFragment).navigateUp()
+    private fun initNavigation() {
+        val navController = findNavController(R.id.navHostFragment)
+
+        // get fragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment)!!
+
+        // setup custom navigator
+        val navigator = CustomNavigator(this, navHostFragment.childFragmentManager, R.id.navHostFragment)
+        navController.navigatorProvider += navigator
+
+        // set navigation graph
+        navController.setGraph(R.navigation.nav_graph_main)
+
     }
 
 }
