68503782b7491cb6c0fd7c5508fcd887c3ed8dc2
alan0313 <alan0987270275@gmail.com>
2021-06-14 12:52:06 +0800

    [Add Feature] add model for API objects.

---
+++ app/src/main/java/com/example/therickandmorty/data/model/Characters.kt
@@ -0,0 +1,5 @@
+package com.example.therickandmorty.data.model
+
+data class Characters(
+    val results: List<Character>
+)
--- app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
+++ app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.therickandmorty.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.plusAssign
 import com.example.therickandmorty.R
-import com.example.therickandmorty.util.CustomNavigator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
