da4db1096f060b17599c8bcfb87d91ad28f89113
alan0313 <alan0987270275@gmail.com>
2021-06-20 21:31:04 +0800

    [Add Feature] make ActionBar title become dynamic using AndroidX Navigation.

--- app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
+++ app/src/main/java/com/example/therickandmorty/view/main/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.therickandmorty.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.therickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Dynamic ActionBar title from a Fragment using AndroidX Navigation
+         */
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+                title = when (destination.id) {
+                    R.id.characterDetailFragment -> "Character"
+                    R.id.episodeFragment -> "Episode"
+                    else -> "The Rick And Morty"
+                }
+        }
+
     }
 }
